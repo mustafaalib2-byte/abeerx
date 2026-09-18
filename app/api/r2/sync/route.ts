eee04a66369c633240b450a3c9ee499cd9661d66
mustafaalib2-byte <mustafaalib2@gmail.com>
2026-09-18 23:00:32 +0530

    456

--- app/api/r2/sync/route.ts
+++ app/api/r2/sync/route.ts
@@ -3,7 +3,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
   region: "auto",
-  endpoint: `https://2604e12e7f799e4e440edaeba8db3d20.r2.cloudflarestorage.com`,
+  endpoint: `https://2604e12a7f799e4e440edaeba8db3d20.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID || "762534b8472c635b562556734a5a3b58",
     secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY || "2397ba96b3235b86776e6c694204906220f384c3db03acf62bffce8696119e8a",
